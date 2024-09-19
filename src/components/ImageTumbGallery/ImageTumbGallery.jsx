37d9698d0e25d73dc019b26d30a6e5b25a481a61
Volodymyr <woland31252@gmail.com>
2024-09-19 20:01:46 +0300

    fix CamperCard

--- src/components/ImageTumbGallery/ImageTumbGallery.jsx
+++ src/components/ImageTumbGallery/ImageTumbGallery.jsx
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux";
 import { selectorsCamperById } from "../../redux/camper/selectorsCamper.js";
-import css from '../ImageTumbGallery/ImageTumbGallery.module.css';
+import css from "../ImageTumbGallery/ImageTumbGallery.module.css";
 
 function ImageTumbGallery() {
   const camper = useSelector(selectorsCamperById);
   return (
     <>
-    
-      {camper.gallery.map((image, index)=>(
-      <img key={index}
-        className={css.imgCamperCard}
-        src={image.thumb}
-        alt="The camper's image"
-      />
+      {camper.gallery.map((image, index) => (
+        <img
+          key={index}
+          className={css.imgCamperCard}
+          src={image.thumb}
+          alt="The camper's image"
+        />
       ))}
     </>
   );
