c808caef8cc755c39fa438783269c4ddaa7116ad
Volodymyr <woland31252@gmail.com>
2024-09-20 17:59:26 +0300

    fix Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { lazy } from "react";
-import Layout from "./Layout/Layout.jsx";
+import Layout from "./temp/temp.jsx";
 const HomePage = lazy(() => import("../pages/Home/HomePage.jsx"));
 const CatalogPage = lazy (()=>import('../pages/Catalog/CatalogPage.jsx'));
 const CamperPage = lazy(() => import('../pages/Camper/CamperPage.jsx'));
