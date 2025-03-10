8e642bc5e60b506223be81710e719f93bd68f258
Volodymyr <woland31252@gmail.com>
2025-03-10 18:34:51 +0200

    create select list to Location.jsx

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -2,15 +2,17 @@ import { useSelector } from "react-redux";
 import { selectorsCampers } from "../../redux/camper/selectorsCamper.js";
 import CamperList from "../CamperList/CamperList.jsx";
 import css from "./Collection.module.css";
+import { useId } from "react";
 
 const Collection = () => {
+  const itemId = useId()
   const campers = useSelector(selectorsCampers);
 
   return (
     <>
       <ul className={css.collectionList}>
         {campers.map((item) => (
-          <li key={item.id}>
+          <li key={itemId}>
             <CamperList item={item} />
           </li>
         ))}
--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,52 +1,35 @@
-// import { Formik, Field } from "formik";
+
 import { useId } from "react";
-import LocationList  from "../LocationList/LocationList.jsx";
-// import { useDispatch, useSelector } from "react-redux";
-// import { selectorsCampers } from "../../redux/camper/selectorsCamper.js";
+import { useSelector } from "react-redux";
+import { selectorsLocation } from "../../redux/camper/selectorsCamper.js";
 import icon from "../../images/icons.svg";
 import css from "./Location.module.css";
-// import { Form } from "react-router-dom";
-
-// const initialValues = { location: "" };
-
-// function Location() {
-//   const locationId = useId();
-//   const dispatch = useDispatch();
-//   const item = useSelector(selectorsCampers);
-//   console.log(item)
-
-//   const handleChange = (values, action) => {
-//     console.log(values);
-//     action.resetForm();
-//   }
-//   return (
-// {/* <Formik initialValues={initialValues} onChange={handleChange}>
-//       <Form>
-//         <label htmlFor={locationId} >Location</label>
-//         <Field as="select" name="location" id={locationId}>
-//           <option value="Ucraine, Kyiv">Ucraine, Kyiv</option>
-//         </Field>
-//       </Form>
-//     </Formik>
-//   );
-// }; */}
 
 function Location() {
   const id = useId();
+  const optionId = useId();
+  const location = useSelector(selectorsLocation);
+  console.log(location);
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
   return (
-    <>
-      <form className={css.location} onChange={()=>{}}>
-        <label className={css.locationLabel} htmlFor={id}>
-          Location
-        </label>
-        <select className={css.inputLocation} id={id} name="location" autoFocus>
-          <LocationList/>
-        </select>
-        <svg className={css.svgIcon}>
-          <use href={`${icon}#icon-map`} />
-        </svg>
-      </form>
-    </>
+    <form className={css.location} onChange={handleChange}>
+      <label className={css.locationLabel} htmlFor={id}>
+        Location
+      </label>
+      <select className={css.inputLocation} id={id} name="location" autoFocus>
+        {location.map((elem) => (
+          <option key={optionId} value={elem}>
+            {elem}
+          </option>
+        ))}
+      </select>
+      <svg className={css.svgIcon}>
+        <use href={`${icon}#icon-map`} />
+      </svg>
+    </form>
   );
 }
 
